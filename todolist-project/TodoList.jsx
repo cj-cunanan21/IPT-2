a10dcf76c67211a442edaa39928d9654a9345462
cj-cunanan21 <148616462+cj-cunanan21@users.noreply.github.com>
2024-03-15 13:29:28 +0800

    Add files via upload

---
+++ todolist-project/TodoList.jsx
@@ -0,0 +1,51 @@
+// TodoList.js
+import React, { useState } from 'react';
+import '../App.css';
+
+const TodoList = () => {
+const [todos, setTodos] = useState([]);
+const [inputValue, setInputValue] = useState('');
+
+const handleChange = (event) => {
+setInputValue(event.target.value);
+  };
+
+const handleSubmit = (event) => {
+event.preventDefault();
+const trimmedValue = inputValue.trim();
+if (trimmedValue !== '') {
+setTodos([...todos, trimmedValue]);
+setInputValue('');
+    }
+  };
+
+const handleDelete = (index) => {
+const newTodos = [...todos];
+newTodos.splice(index, 1);
+setTodos(newTodos);
+  };
+
+return (
+    <div className="todo-list">
+      <h1 >Carl James  TodoList</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Add a new todo"
+        /> <button className='rengar' type="submit">Add To Do</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button className='kindred' onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+           ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
